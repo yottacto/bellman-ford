07a92512826a1a2dca6a35b81f21a167eb33920d
yottacto <yotta.w.cto@gmail.com>
2019-01-13 14:43:18 +0800

    Add config toml

--- mpi/src/main.cc
+++ mpi/src/main.cc
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "sssp.hh"
 #include "util.hh"
+#include "config.hh"
 
 int main()
 {
-    icesp::sssp bf{"../dataset/USA-road-d.NY.gr"};
-    // icesp::sssp bf{"../dataset/USA-road-d.USA.gr"};
-    // icesp::sssp bf{"../dataset/USA-road-d.CAL.gr"};
-    // bf.print();
-    // duration(bf.rank, [&]() { bf.compute(0, 2000000); });
-    icesp::duration(bf.rank, [&]() { bf.compute<true>(0, 200000); });
+    auto config{icesp::config{}};
+    auto path = config.front().path;
+    auto s = config.front().s;
+    auto t = config.front().t;
+
+    icesp::sssp bf{path};
+    icesp::duration(bf.rank, [&]() { bf.compute<true>(s, t); });
     bf.print_statistic();
 }
 
